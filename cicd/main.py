73c91b9bf0084dd36200dd2836dd714c73a29719
viojha96 <vivekojhabackup1996@gmail.com>
2025-10-29 05:37:34 +0000

    cicd commit

---
+++ cicd/main.py
@@ -0,0 +1,16 @@
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    """Example Hello World route."""
+    image_tag = os.getenv("APP_IMAGE_TAG", "unknown")
+    return f"Hello, world! I'm running an updated app version: {image_tag}"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
